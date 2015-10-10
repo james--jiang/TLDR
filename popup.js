26eeeb9abd0a34feed0440d58978d46128416254
Tommy Huynh <TommysMAC@airbears2-10-142-22-105.AirBears2.1918.Berkeley.EDU>
2015-10-10 08:18:30 -0700

    initial code: parse text complete

---
+++ popup.js
@@ -0,0 +1,21 @@
+// This callback function is called when the content script has been 
+// injected and returned its results
+function onPageDetailsReceived(pageDetails)  { 
+    document.getElementById('title').textContent = pageDetails.title; 
+    document.getElementById('url').textContent = pageDetails.url; 
+    document.getElementById('summary').innerText = pageDetails.summary; 
+} 
+
+var statusDisplay = null;
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    
+    // Get the event page/background page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in 
+        // our onPageDetailsReceived function as the callback. This injects 
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
