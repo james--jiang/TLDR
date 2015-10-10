5b9f72a0860382cdb7a03edd1314ed1eae98a3fe
Jack Sullivan <jsullivan27@berkeley.edu>
2015-10-10 13:18:49 -0700

    Working extension.js?

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 var word_map = {};
 var add_words = function(sentence_array) { 
-	for (sentence in sentence_array) {
+    for (sentence in sentence_array) {
 		for (word in sentence.split(" ")) {
 			var newWord = stemmer(removeContraction(word))
 			var contraction = newWord.split(" ")
@@ -18,17 +18,21 @@ var add_words = function(sentence_array) {
 				        nextWord.substring(0, nextWord.length - 1);
 				    }
 				}
-				if (!word_map.contains(nextWord)) {
+				if (!(nextWord in word_map)) {
 					word_map[nextWord] = 0;
 				}
-				word_map[nextWord] += 1
+				word_map[nextWord] = word_map[nextWord] + 1;
 			}
 		}	
 	}
 }
 
 var norms = ['is', 'and', 'a', 'the', 'that', 'are', 'in', 'an', 'be', 'to', 'of', 'for', 'he', 'she', 'they', 'not', 'as', 'but', 'his', 'her', 'or', 'nor', 'if', 'so', 'its', 'than', 'then', 'were', 'was'];
-var title =  "Fahad didnt help on the extension.";//CHANGE TITLE
+var title_arr = [];
+
+var clean_title = function(og_title) {
+var title =  "Fahad didnt help on the extension.".split(" ");//inputted_title.split(" ");
+
 var title_length = title.length;
 var index = 0;
 for (word_seg in title) {
@@ -48,23 +52,28 @@ for (word_seg in title) {
 	        word_seg.substring(0, nextWord.length - 1);
 	    }
     }
-    title[index] = word_seg;
+    title_arr[index] = word_seg;
     index = index + 1;
     
 }
-var update_score = function(word) {
+return title_arr;
+}
+
+
+var update_score = function(word) { //applied where?
 	var def = 1;
-	if (title.contains(word)) {
+	if (title_arr.includes(word)) {
 		def = 1.5;
 	}
-	if (norms.contains(word)) {
+	if (norms.includes(word)) {
 		def = .5;
 	}
-	map[word] = map[word] * def;
+	map[word] = map[word] * def; // what map
 }
 
 var sentMap = {};
 var sum_sentence = function(sentenceArray) { 
+    var index =0;
 	for (sent in sentenceArray) {
 		var sum = 0;
 		var words = sent.split(" ");
@@ -82,10 +91,10 @@ var sum_sentence = function(sentenceArray) {
 			        word.substring(0, nextWord.length - 1);
 			    }
 			} 
-			if (word_map.contains(word)) {
+			if (word in word_map) {
 				sum += word_map[nextWord];
 			} else {
-				if (norms.contains(word)) {
+				if (word in norms) {
 					sum += .5;
 				}
 				else {
@@ -93,7 +102,8 @@ var sum_sentence = function(sentenceArray) {
 				}
 			}
 		}
-		sentMap[sentenceArray.indexOf(sent)] = sum;
+		sentMap[index] = sum;
+        index += 1;
 	}
 }
 
@@ -101,7 +111,7 @@ var sum_sentence = function(sentenceArray) {
 
 print_final = function(sentenceArray) {
 	var sortable = [];
-	for (var index in sentMap) {
+	for (index in sentMap) {
 	    sortable.push([index, sentMap[index]]);
 	}
     sortable.sort(function(a, b) {return b[1] - a[1]});
@@ -111,6 +121,7 @@ print_final = function(sentenceArray) {
 
 	while (i < 7 && i < sortable.length) {
 		str = str.concat((sentenceArray[sortable[i][0]]));
+        i = i + 1;
 	}
 	return str;
 }
